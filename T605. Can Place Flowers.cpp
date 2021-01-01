e18436693ba966a3cbc60aa3d990cceb8a49070e
PluckySaltyfish <1828151761@qq.com>
2021-01-01 22:08:58 +0800

    Easy

--- T605. Can Place Flowers.cpp
+++ T605. Can Place Flowers.cpp
@@ -1,36 +1,16 @@
 class Solution {
 public:
-    int max_flower(int x){
-        if(x < 0)
-            return 0;
-        return (x+1)/2;
-    }
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int last = -1;
-        int ans = 0;
-        int len = flowerbed.size();
-        for(int i = 0;i < len;i++){
-            if(flowerbed[i] == 1){
-                if(last == -1)
-                    ans += max_flower(i - 1 - 0);
-                else
-                    ans += max_flower((i - 1) - (last + 1) - 1);
-                last = i;
-                if(ans >= n)
-                    return true;
+        int cnt = 0;
+        int fn = flowerbed.size();
+        for(int i = 0;i < fn; ++i){
+            if(flowerbed[i]==0){
+                if((i==0||flowerbed[i-1]==0)&&((i==fn-1)||flowerbed[i+1]==0)){
+                    cnt++;
+                    flowerbed[i] = 1;
+                }
             }
         }
-        if(flowerbed[len - 1] == 1)
-            if(last == -1) ans += max_flower(len - 1);
-            else 
-                ans += max_flower((len - 1 - 1) - (last + 1) - 1);
-        else{
-            if(last == -1) ans += max_flower(len);
-            else
-                ans += max_flower((len - 1) - (last + 1));
-        }
-        if(ans >= n)
-            return true;
-        return false;
+        return cnt>=n;
     }
 };
