02f14a8ef0f7e0264baa3d60194e55873cda43a4
PluckySaltyfish <1828151761@qq.com>
2021-02-08 12:32:48 +0800

    DP;遍历

--- T978. Longest Turbulent Subarray.cpp
+++ T978. Longest Turbulent Subarray.cpp
@@ -1,26 +1,46 @@
+//状态记录，一次遍历
 class Solution {
 public:
-    int maxTurbulenceSize(vector<int>& A) {
-        if(A.size()==1) return 1;
-        int flag = A[0] > A[1];
-        int dp[40000];
-        dp[0] = 1;
-        dp[1] = A[0] == A[1] ? 1 : 2;
-        int len = dp[1];
-        for (int i = 2; i < A.size(); i++) {
-            if(A[i - 1] == A[i]){
-                len = len > dp[i - 1] ? len : dp[i - 1];
-                dp[i] = 1;
+    int maxTurbulenceSize(vector<int>& arr) {
+        int n = arr.size(),flag = 0,len = 1,max_len = 1;
+        if(n == 1)return 1;
+        for(int i = 1;i < n; ++i){
+            if(arr[i] == arr[i-1]){
+                flag = 0;
+                len = 1;
             }
-            else if((A[i - 1] > A[i]) ^ flag)
-                dp[i] = dp[i - 1] + 1;
-            else{
-                len = len > dp[i - 1] ? len : dp[i - 1];
-                dp[i] = 2;
+            else {
+                if(flag==0||((arr[i]-arr[i-1])^flag)< 0)//异或判断两个数符号是否相等
+                    len++;
+                else len = 2;
+                flag = arr[i]-arr[i-1];
+                max_len = max(len,max_len);
             }
-            flag = A[i - 1] > A[i];
         }
-        len = len > dp[A.size() - 1] ? len : dp[A.size() - 1];
-        return len;
+        return max_len;
+    }
+};
+
+//O(1)空间的DP，分别用两个dp记录以i结尾上升，下降湍流数组的长度，互相辅助更新
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& arr) {
+        int n = arr.size(),ans = 1,dp1 = 1,dp2 = 1;
+        for(int i = 1;i < n; ++i){
+            if(arr[i] == arr[i-1]){
+                dp1 = dp2 = 1;
+            }
+            else if(arr[i] > arr[i-1]){
+                dp2 = dp1 + 1;
+                dp1 = 1;
+                ans = max(ans,dp2);
+            }
+            else{
+                dp1 = dp2 + 1;
+                dp2 = 1;
+                ans = max(ans,dp1);
+            } 
+        }
+        return ans;
     }
 };
